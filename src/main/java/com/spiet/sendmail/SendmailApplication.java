052e7fdc0a225fdc5fbd7672d5eb4291119bd90e
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-24 21:47:54 -0300

    scheduling services with spring scheduling

--- src/main/java/com/spiet/sendmail/SendmailApplication.java
+++ src/main/java/com/spiet/sendmail/SendmailApplication.java
@@ -4,13 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class SendmailApplication {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Scheduled(cron = "0 47 21 1/1 * ?")
+    private void teste() {
+        System.out.println("TESTANDOOOO!");
     }
 
     public static void main(String[] args) {
